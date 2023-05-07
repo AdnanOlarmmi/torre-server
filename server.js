e2c0e6336334ceca93bfa024c7dfa3dd5627f00f
AdnanOlarmmi <olarmmi@gmail.com>
2023-05-07 21:45:17 +0100

    Add access control allow origin

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://dev--cheerful-maamoul-ba5e46.netlify.app/');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
