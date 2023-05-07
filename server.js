1690dd12b330e11fafb64da0243af7097b8c6aa5
AdnanOlarmmi <olarmmi@gmail.com>
2023-05-07 21:14:09 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const request = require('request');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.get('/bios/:username', (req, res) => {
+  const { username } = req.params;
+  const url = `https://torre.bio/api/bios/${username}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log(error);
+      return res.status(500).send({ error: 'Error retrieving user data' });
+    }
+    const data = JSON.parse(body);
+    res.status(200).send(data);
+  });
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
